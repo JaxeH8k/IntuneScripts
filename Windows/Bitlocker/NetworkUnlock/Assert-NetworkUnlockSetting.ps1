a5c871bdc816dbb2d337767eef553bd821a7a13f
JaxeH8k <jh@dangeroustoysforkids.com>
2025-08-17 20:26:37 -0700

    add basic logging to assert-networkUnlock script

--- Windows/Bitlocker/NetworkUnlock/Assert-NetworkUnlockSetting.ps1
+++ Windows/Bitlocker/NetworkUnlock/Assert-NetworkUnlockSetting.ps1
@@ -5,12 +5,54 @@
     [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes($filePath)) | clip
 
 #>
-$base64Cert = '' # your base64 encoded certificate (see comment above)
-$certBytes = [System.Convert]::FromBase64String($base64Cert)
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList (,$certBytes)
 
-# Construct the GPO-style Blob (header + prefix (+ cert length) + certificate data)
-$certData = $cert.RawData
+$base64Cert = '' # your base64 encoded certificate (see comment above)
+
+# logging
+function Write-Log {
+    param(
+        [string]$message,
+        [string]$logLevel = "Info"
+    )
+
+    $eventTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss.ffff"
+    "$eventTime | $logLevel | $message" | Tee-Object -FilePath $logPath -Append
+}
+
+$logLocation = "C:\programdata\DGTK\Bitlocker\Enable-NetworkUnlock $(get-date -format 'yyyy-MM-dd HH:mm:ss').log"
+# test if parent directory exists
+if(! (Test-Path (Split-Path $logLocation -Parent))){
+    New-Item (Split-Path $logLocation -Parent) -ItemType Directory -Force
+}
+
+Write-Log "---Begin Enablement of Network Unlock Script---"
+
+$certBytes = [System.Convert]::FromBase64String($base64Cert)
+if($?){
+    Write-Log 'Converted public network unlock script from base64 string to byte array'
+}else{
+    Write-Log "$error[0].ErrorMessage" -logLevel "Error"
+    Exit 1
+}
+try{
+    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList (,$certBytes) -ErrorAction Stop
+    Write-Log 'Converted byte array to x509 certificate object'
+}catch{
+    Write-Log "Failed to convert byte array to x509 certificate object"
+    Write-Log "$_" -logLevel "Error"
+    Exit 1
+}
+
+Write-Log "Construct GPO registry blob representing the certificate (header + thumbrint bytes + prefix (+ cert length) + certificate data)"
+if( $cert.PSObject.Properties["RawData"]){
+    Write-Log 'X509 Certificate Contains Data.  Proceed...'
+    $certData = $cert.RawData
+}else{
+    Write-Log 'X509 Certificate Object Contains no Data. Exit'
+    Exit 1
+}
+
+Write-Log 'X509 cert succesfully contructed. Proceed to construct registry blob'
 $certLength = $certData.Length
 $certLengthBytes = [BitConverter]::GetBytes($certLength)
 $Header = [byte[]]@(0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00, 0x00)
@@ -30,7 +72,25 @@ $regPaths = @(
     "HKLM:\SOFTWARE\Policies\Microsoft\SystemCertificates\FVE_NKP\CTLs"
 )
 foreach($regPath in $regPaths){
-    New-Item -Path $regPath -Force
+    Write-Log 'Create Regsitry Paths for Network Unlock'
+    try{
+        New-Item -Path $regPath -Force -ErrorAction Stop
+        Write-Log "Created $regPath succesfully."
+    }catch{
+        Write-Log "$_" -logLevel 'Error'
+    }
 }
-Set-ItemProperty -Path $regPaths[0] -Name "Blob" -Value $Blob -Type Binary
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSManageNKP" -Value 1 -Type DWord
+try{
+    Set-ItemProperty -Path $regPaths[0] -Name "Blob" -Value $Blob -Type Binary -ErrorAction Stop
+    Write-Log 'Succesfully wrote certificate binary data to registry'
+}catch{
+    Write-Log "$_" -logLevel 'Error'
+    Exit 1
+}
+try{
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\FVE" -Name "OSManageNKP" -Value 1 -Type DWord -ErrorAction Stop
+    Write-Log 'Succefully wrote registry value for gpo setting "Allow Network Unlock"'
+}catch{
+    Write-Log "$_" -logLevel 'Error'
+    Exit 1
+}
